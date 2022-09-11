60771a5fd4597c312504fc61b22eb3ac8b588146
Duy Khang <nguyenduykhangtvh12@gmail.com>
2022-09-11 17:07:55 +0700

    Da_hoan_thanh

--- 6_Thuat_Toan_Sap_Xep.cpp
+++ 6_Thuat_Toan_Sap_Xep.cpp
@@ -53,3 +53,89 @@ void Bubble_Sort(int a[], int n)
 			swap(a[j], a[j-1]);
 }
 
+void QuickSort(int a[], int left, int right)
+{
+    int i, j, x;
+    if (left >= right)
+    {
+        return;
+    }
+    x = a[(left+right)/2]; 
+    i = left; j = right;
+    while(i < j) {
+        while(a[i] < x){
+            i++;
+        }
+        while(a[j] > x){
+            j--;
+        }
+        if(i <= j) {
+            swap(a[i], a[j]);
+            i++ ;
+            j--;
+        }
+    }
+    QuickSort(a, left, j);
+    QuickSort(a, i, right);
+}
+
+void HeapSort (int a[], int left, int right)
+{
+    int x, i, j;
+    i = left; 
+    j =2*i+1; 
+    x = a[i];
+    while (j <= right)
+    {
+        if (j < right)
+        { 
+            if (a[j] < a[j+1])
+            {
+                j = j+1;
+            }
+        }
+        if (a[j]<x)
+        {
+            break; 
+        }
+        a[i] = a[j];
+        i = j; 
+        j = 2*i+1;
+    }
+    a[i] = x;
+}
+
+void Xuatmang(int a[], int n)
+{
+	for(int i = 0; i<n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
+
+int main()
+{
+    int a[] = {12, 2, 8, 10, 1, 6, 4, 15};
+	int N=8; 
+    
+    Xuatmang(a,N);
+	printf("\nSelectionSort: ");
+	SelectionSort(a,N);
+	Xuatmang(a,N);
+	printf("\nInsertionSort: ");
+	InsertionSort(a, N);
+	Xuatmang(a,N);
+	printf("\nInterchange_Sort: ");
+	Interchange_Sort(a,N);
+	Xuatmang(a,N);
+	printf("\nBubble_Sort: ");
+	Bubble_Sort(a, N);
+	Xuatmang(a,N);
+    printf("\nQuickSort: ");
+	QuickSort(a,0, N);
+	Xuatmang(a,N);
+    printf("\nHeapSort: ");
+	HeapSort(a, 0, N);
+	Xuatmang(a,N);
+}
+
