e26c3d1962a8d02d5592e134d4d1e40db709be37
nguyenhuuluan1702 <nguyenhuuluantvtc@gmail.com>
2022-09-11 12:46:05 +0700

    Hu

--- nhapmonbaocao.cpp
+++ nhapmonbaocao.cpp
@@ -1,35 +1,37 @@
 #include<stdio.h>
 
-void swap(int &xp, int &yp)
-{
+void swap(int & xp, int & yp) {
     int temp = xp;
     xp = yp;
     yp = temp;
 }
-void SelectionSort(int arr[], int n)
-{
-    int i, j, min;
-    for (i = 0; i < n-1; i++)
-    {
-    min = i;
-    for (j = i+1; j < n; j++)
-        if (arr[j] < arr[min])
-        min = j;
- 
-        swap(arr[min], arr[i]);
-    }
-void InsertionSort(int a[], int n)
-	{	
+void SelectionSort(int arr[], int n) {
+        int i, j, min;
+        for (i = 0; i < n - 1; i++) {
+            min = i;
+            for (j = i + 1; j < n; j++)
+                if (arr[j] < arr[min])
+                    min = j;
+
+            swap(arr[min], arr[i]);
+        }
+}
+
+void InsertionSort(int arr[], int n) {
 	int pos, x;
-	for(int i = 1; i < n; i++)
-	{ 
-		x = a[i]; 
+	for (int i = 1; i < n; i++) {
+		x = arr[i];
 		pos = i;
-		while(pos > 0 && x < a[pos-1])
-		{
-			a[pos] = a[pos-1];	
+		while (pos > 0 && x < arr[pos - 1]) {
+			arr[pos] = arr[pos - 1];
 			pos--;
 		}
-		a[pos] = x;
+		arr[pos] = x;
 	}
+}
+
+int main()
+{
+	return 0;
+
 }
